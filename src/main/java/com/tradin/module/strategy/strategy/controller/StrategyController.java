44c7299dc5204c13026e1405371429b0e238fc24
JiHwan <sgh002400@naver.com>
2025-05-02 22:59:50 +0900

    chore: 레거시 로직 삭제

--- src/main/java/com/tradin/module/strategy/strategy/controller/StrategyController.java
+++ src/main/java/com/tradin/module/strategy/strategy/controller/StrategyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1/strategies")
 public class StrategyController implements StrategyApi {
+
     private final StrategyService strategyService;
 
     @GetMapping("/future")
@@ -24,21 +25,4 @@ public class StrategyController implements StrategyApi {
         return strategyService.findSpotStrategiesInfo();
     }
 
-//    @Operation(summary = "선물 전략 구독 리스트")
-//    @GetMapping("/subscriptions")
-//    public FindSubscriptionStrategiesInfoResponseDto findSubscriptionStrategiesInfos() {
-//        return strategyService.findSubscriptionStrategiesInfo();
-//    }
-//
-//    @Operation(summary = "선물 전략 구독")
-//    @PostMapping("/{id}/subscriptions")
-//    public void subscribe(@Valid @RequestBody SubscribeStrategyRequestDto request, @PathVariable Long id) {
-//        strategyService.subscribeStrategy(request.toServiceDto(id));
-//    }
-//
-//    @Operation(summary = "선물 전략 구독 취소")
-//    @PatchMapping("/unsubscriptions")
-//    public void unsubscribe(@Valid @RequestBody UnSubscribeStrategyRequestDto request) {
-//        strategyService.unsubscribeStrategy(request.toServiceDto());
-//    }
 }
