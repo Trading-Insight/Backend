e6b183fe370b1ed1b997ff9a47993169eda21ac2
JiHwan <62228195+sgh002400@users.noreply.github.com>
2025-09-13 14:47:50 +0900

    [FIX] Default 분산락 대기 시간 5 -> 2초로 감축
    Updated the default wait time for the lock from 5 seconds to 2 seconds.

--- Tradin-Core/src/main/java/com/tradin/core/common/annotation/DistributedLock.java
+++ Tradin-Core/src/main/java/com/tradin/core/common/annotation/DistributedLock.java
@@ -21,9 +21,9 @@ public @interface DistributedLock {
     TimeUnit timeUnit() default TimeUnit.SECONDS;
 
     /**
-     * 락을 기다리는 시간 (default - 5s) 락 획득을 위해 waitTime 만큼 대기한다
+     * 락을 기다리는 시간 (default - 2s) 락 획득을 위해 waitTime 만큼 대기한다
      */
-    long waitTime() default 5L;
+    long waitTime() default 2L;
 
     /**
      * 락 임대 시간 (default - 3s) 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다
