879513babaad3f6671d1dd2558d6ac9c95adf25b
JiHwan <sgh002400@naver.com>
2025-10-03 21:55:29 +0900

    feat: watchdog을 사용하도록 수정, leaseTime 초과 시 트랜잭션 롤백하도록 수정

--- Tradin-Core/src/main/java/com/tradin/core/common/aop/AopForTransaction.java
+++ Tradin-Core/src/main/java/com/tradin/core/common/aop/AopForTransaction.java
@@ -1,14 +1,21 @@
 package com.tradin.core.common.aop;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AopForTransaction {
+    private final PlatformTransactionManager transactionManager;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
@@ -22,8 +29,36 @@ public class AopForTransaction {
                 throwable
             );
             throw throwable;
-        } finally {
-//            log.info("🔓 트랜잭션 종료 - method: {}", joinPoint.getSignature().getName());
+        }
+    }
+
+    /**
+     * 트랜잭션을 새로 생성하고, 지정된 시간(초) 내에 완료되지 않으면 롤백합니다.
+     */
+    public Object proceedWithTimeout(final ProceedingJoinPoint joinPoint, int timeoutSeconds) throws Throwable {
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+        transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        if (timeoutSeconds > 0) {
+            transactionDefinition.setTimeout(timeoutSeconds);
+        }
+
+        TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefinition);
+
+        try {
+            Object result = joinPoint.proceed();
+            transactionManager.commit(transactionStatus);
+            return result;
+        } catch (Throwable throwable) {
+            transactionManager.rollback(transactionStatus);
+            log.error(
+                "🚫 트랜잭션 처리 중 오류 발생 - method: {}, timeout: {}s, error: {}",
+                joinPoint.getSignature().getName(),
+                timeoutSeconds,
+                throwable.getMessage(),
+                throwable
+            );
+            throw throwable;
         }
     }
 }
